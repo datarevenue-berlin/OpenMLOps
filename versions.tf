48f0a66cfc98a4065bcfcaea835f3ae647172468
Michał Dankiewicz <michal@datarevenue.com>
2021-02-02 15:09:12 +0100

    Loosen AWS provider version
    Otherwise it conflicts with other providers and terraform
    gives an error while initialising.
    
    #time 2m

--- versions.tf
+++ versions.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.20"
+      version = "~> 3.20"
     }
     http = {
       source = "hashicorp/http"
