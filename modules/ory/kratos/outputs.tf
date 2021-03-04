110a8b562eb83712b3303002edfffcd234db7920
Thales <tvinagre@gmail.com>
2021-03-04 10:56:18 -0300

    Cleanup rendering yaml

--- modules/ory/kratos/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "oidc_providers_rendered" {
-  value =  join("\n", data.template_file.oidc-providers.*.rendered)
-}
--- modules/ory/main.tf
+++ modules/ory/main.tf
@@ -1,18 +1,18 @@
-module "ory-oathkeeper" {
-  source = "./oathkeeper"
-  namespace = var.namespace
-
-  hostname = var.hostname
-  protocol = var.protocol
-}
-
-module "ory-kratos" {
-  source = "./kratos"
-  namespace = var.namespace
-
-  cookie_secret = var.cookie-secret
-  db_password = var.db_password
-  oauth2_providers = var.oauth2_providers
-
-  domain = "${var.protocol}://${var.hostname}"
-}
+//module "ory-oathkeeper" {
+//  source = "./oathkeeper"
+//  namespace = var.namespace
+//
+//  hostname = var.hostname
+//  protocol = var.protocol
+//}
+//
+//module "ory-kratos" {
+//  source = "./kratos"
+//  namespace = var.namespace
+//
+//  cookie_secret = var.cookie-secret
+//  db_password = var.db_password
+//  oauth2_providers = var.oauth2_providers
+//
+//  domain = "${var.protocol}://${var.hostname}"
+//}
