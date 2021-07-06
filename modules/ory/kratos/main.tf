48fdf16251109b32d9ada052c7dea57e2b7a6870
bernardolk <bernardo.knackfuss@gmail.com>
2021-07-06 17:48:40 -0300

    verification email and keto authorization related configs

--- modules/ory/kratos/main.tf
+++ modules/ory/kratos/main.tf
@@ -71,7 +71,7 @@ resource "kubernetes_deployment" "ory-kratos-ui" {
       spec {
         container {
           name = "ory-kratos-ui"
-          image = "oryd/kratos-selfservice-ui-node:v0.6.3-alpha.1"
+          image = "oryd/kratos-selfservice-ui-node:v0.6.0-alpha.2"
           env {
             name = "KRATOS_PUBLIC_URL"
             value = "http://${helm_release.ory-kratos.name}-public.${var.namespace}.svc.cluster.local:80"
