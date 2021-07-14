d6b2ee23ca571352e130c4434b7232b7695fc6a8
bernardolk <bernardo.knackfuss@gmail.com>
2021-07-14 11:12:53 -0300

    added values.yaml to seldon module + storage init image change from default

--- modules/seldon/main.tf
+++ modules/seldon/main.tf
@@ -5,8 +5,7 @@ resource "helm_release" "seldon" {
   repository = "https://storage.googleapis.com/seldon-charts"
   chart      = "seldon-core-operator"
 
-  set {
-    name  = "usageMetrics.enabled"
-    value = var.usage_metrics_enabled
-  }
+  values = [templatefile("${path.module}/values.yaml", {
+    usage_metrics_enabled = var.usage_metrics_enabled
+  })]
 }
