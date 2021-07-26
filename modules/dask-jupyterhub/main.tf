4629195c29b07b000e9d90c1af5b75576691fffb
bernardolk <bernardo.knackfuss@gmail.com>
2021-07-26 13:47:40 -0300

    testing

--- modules/dask-jupyterhub/main.tf
+++ modules/dask-jupyterhub/main.tf
@@ -6,4 +6,8 @@ resource "helm_release" "dask-jupyterhub" {
   chart      = "daskhub"
   version    = "2021.7.2"
 
+  values = [templatefile("${path.module}/values.yaml", {
+    jupyterhub_secret = var.jupyterhub_secret
+    daskgateway_secret = var.daskgateway_secret
+  })]
 }
---
+++ modules/dask-jupyterhub/variables.tf
@@ -0,0 +1,10 @@
+
+variable "jupyterhub_secret" {
+    type = string
+    default = "4301bf5a2aa1fbade157046863ac64ec46df03e3da39ec3bf345a2f8caa81e02"
+}
+
+variable "daskgateway_secret" {
+    type = string
+    default = "ca7de235a4ae54103d49f5004a11690004c66fe14810f35dc476103573e56ff1"
+}
