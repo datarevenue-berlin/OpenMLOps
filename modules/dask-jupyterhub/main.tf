00ff682eabfdeb7eb55e52fd94b213583e3a7184
bernardolk <bernardo.knackfuss@gmail.com>
2021-07-27 15:32:41 -0300

    missing var passing into values.yaml

--- modules/dask-jupyterhub/main.tf
+++ modules/dask-jupyterhub/main.tf
@@ -9,5 +9,11 @@ resource "helm_release" "dask-jupyterhub" {
   values = [templatefile("${path.module}/values.yaml", {
     jupyterhub_secret = var.jupyterhub_secret
     daskgateway_secret = var.daskgateway_secret
+    singleuser_profile_list = var.singleuser_profile_list
+    singleuser_image_pull_secrets = var.singleuser_image_pull_secrets
+    singleuser_image_pull_policy = var.singleuser_image_pull_policy
+    singleuser_memory_guarantee = var.singleuser_memory_guarantee
+    singleuser_storage_capacity = var.singleuser_storage_capacity
+    singleuser_storage_mount_path = var.singleuser_storage_mount_path
   })]
 }
