3fc20aa2e8fe0dd6fc96a951add8a61d47f64784
bernardolk <bernardo.knackfuss@gmail.com>
2021-07-27 14:04:36 -0300

    helm version upgrate

--- versions.tf
+++ versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.0.1"
+      version = "~> 2.2.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
