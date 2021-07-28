95acbf5cb17522db3ee1889a5488398c82ac8474
bernardolk <bernardo.knackfuss@gmail.com>
2021-07-28 17:19:26 -0300

    temporarily removing daskhub deployment

--- main.tf
+++ main.tf
@@ -29,16 +29,31 @@
 //  singleuser_profile_list = var.singleuser_profile_list
 //}
 
-resource "kubernetes_namespace" "daskhub_namespace" {
-  metadata {
-    name = "daskhub"
-  }
-}
 
 
-module "dask-jupyterhub" {
-    source    = "./modules/dask-jupyterhub"
-}
+
+
+
+
+
+
+//resource "kubernetes_namespace" "daskhub_namespace" {
+//  metadata {
+//    name = "daskhub"
+//  }
+//}
+
+
+//module "dask-jupyterhub" {
+//    source    = "./modules/dask-jupyterhub"
+//}
+
+
+
+
+
+
+
 
 
 resource "kubernetes_namespace" "mlflow_namespace" {
