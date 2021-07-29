511e180c685b0e1d71f42f0398d3a8590cb5f08e
bernardolk <bernardo.knackfuss@gmail.com>
2021-07-29 18:47:13 -0300

    dask deployment changes

--- modules/dask/main.tf
+++ modules/dask/main.tf
@@ -7,67 +7,17 @@ resource "helm_release" "dask" {
   chart      = "dask"
   version    = "2021.7.0"
 
-  set {
-    name  = "worker.name"
-    value = var.worker_name
-  }
-
-  set {
-    name  = "worker.replicas"
-    value = var.worker_replicas
-  }
-
-  set {
-    name  = "worker.image.repository"
-    value = var.worker_image_repository
-  }
-
-  set {
-    name  = "worker.image.tag"
-    value = var.worker_image_tag
-  }
-
-  set {
-    name  = "worker.image.pullPolicy"
-    value = var.worker_image_pull_policy
-  }
-
-  set {
-    name  = "worker.image.dask_worker"
-    value = var.worker_image_dask_worker_command
-  }
-
-  set {
-    name  = "scheduler.image.repository"
-    value = var.scheduler_image_repository
-  }
-
-  set {
-    name  = "scheduler.image.tag"
-    value = var.scheduler_image_tag
-  }
-
-  set {
-    name  = "scheduler.image.pullPolicy"
-    value = var.scheduler_image_pull_policy
-  }
-
-
-  values = [
-    yamlencode({
-      "worker" = {
-        "env" = var.worker_environment_variables
-      }
-    }),
-    yamlencode({
-      "worker" = {
-        "image" = {
-          "pullSecrets" = var.worker_image_pull_secret
-        }
-      }
-    })
-  ]
-
-
-
+  values = [templatefile("${path.module}/values.yaml", {
+    worker_name = var.worker_name
+    worker_replicas = var.worker_replicas
+    worker_environment_variables = var.worker_environment_variables
+    worker_image_repository = var.worker_image_repository
+    worker_image_tag = var.worker_image_tag
+    worker_image_pull_policy = var.worker_image_pull_policy
+    worker_image_pull_secret = var.worker_image_pull_secret
+    worker_image_dask_worker_command = var.worker_image_dask_worker_command
+    scheduler_image_repository = var.scheduler_image_repository
+    scheduler_image_tag = var.scheduler_image_tag
+    scheduler_image_pull_policy = var.scheduler_image_pull_policy
+  })]
 }
